c05da4a72c8889267dea6da5bd1d499187448183
Ayush <84891062+Ayush945@users.noreply.github.com>
2023-11-10 11:17:32 +0545

    Added frontend and backend of website

---
+++ Backend/Fruit-Flow/src/main/java/com/example/FruitFlow/FruitFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.FruitFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitFlowApplication.class, args);
+	}
+
+}
---
+++ Backend/Fruit-Flow/src/test/java/com/example/FruitFlow/FruitFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FruitFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
